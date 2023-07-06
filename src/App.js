7f5f3535a58d551fed5a6090bf60be166601de38
student <joycemumbiiwanjiku@gmail.com>
2023-07-06 08:20:04 +0300

    Route-react

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Login from './LoginPage';
+// import Navbar from './NavbarPage';
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Products from './Products';
+// import ProductDetailsPage from './ProductsDetsPage';
+// import AddProductForm from './AddProduct';
+function App() {
+  return (
+    <div >
+      {/* <Navbar/> */}
+<BrowserRouter>
+<Routes>
+  <Route path='/login' element ={<Login/>}></Route>
+  {/* <Route path='productdetails' element={<ProductDetailsPage/>}/> */}
+  {/* <Route path='/product/:productId' element ={<ProductDetailsPage/>}/> */}
+  <Route path='/products' element ={<Products/>}/>
+  {/* <Route path ='/AddProduct' element={<AddProductForm/>}/> */}
+</Routes>
+</BrowserRouter>
+    </div>
+  );
+}
+export default App;
